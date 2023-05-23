13ba7037ec8f1dca9df81430dd544dabf1d92ae8
Hegnon <hegnondutra@gmail.com>
2023-05-23 07:25:50 -0300

    corrigindo erros teste binary search

--- binary_search/spec/binary_search_spec.rb
+++ binary_search/spec/binary_search_spec.rb
@@ -3,4 +3,12 @@ require 'binary_search'
 RSpec.describe BinarySearch do
     describe 'binary_search' do
         it 'a simple binary search' do
+            arr = [10,20,30,40,50,60,70,80,90]
+            target = 90
+            expect(result).to eql(8) 
+
+            result = binary_search(arr, target)
+            expect(result).to eq(expected_output)
+        end
     end
+end    
