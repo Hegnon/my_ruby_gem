cfdc94e0d1b3bf827c0f94f33dde91a8627632a3
Hegnon <hegnondutra@gmail.com>
2023-05-23 06:54:25 -0300

    refactory calculate age

--- calculate_age/spec/calculate_age_spec.rb
+++ calculate_age/spec/calculate_age_spec.rb
@@ -2,7 +2,7 @@ require 'calculate_age'
 
 
 RSpec.describe CalculateAge do
-    describe '.calculate_idade' do
+    describe 'calculate_idade' do
         it 'calculates the correct age' do
         data_nascimento_str = '00/00/0000'
         idade_anos, idade_meses, idade_dias = CalculateAge.calculate_idade(data_nascimento_str)
