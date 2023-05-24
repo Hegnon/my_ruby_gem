1d64fad4c1dd56b71b07179575aadeec99502b41
Hegnon <hegnondutra@gmail.com>
2023-05-24 07:14:20 -0300

    erros corrigidos, teste ok.

--- binary_search/lib/binary_search.rb
+++ binary_search/lib/binary_search.rb
@@ -29,5 +29,5 @@ module BinarySearch
   arr = [10,20,30,40,50,60,70,80,90]
   target = 90
 
-  puts binary_search(arr,target)
+  
 end
--- binary_search/spec/binary_search_spec.rb
+++ binary_search/spec/binary_search_spec.rb
@@ -1,14 +1,16 @@
 require 'binary_search'
 
 RSpec.describe BinarySearch do
+    include BinarySearch
+
     describe 'binary_search' do
         it 'a simple binary search' do
-            arr = [10,20,30,40,50,60,70,80,90]
-            target = 90
-            expect(result).to eql(8) 
+        arr = [10,20,30,40,50,60,70,80,90]
+        target = 90
+        expected_output = 8
 
-            result = binary_search(arr, target)
-            expect(result).to eq(expected_output)
+        result = binary_search(arr, target)
+        expect(result).to eql(expected_output)          
         end
     end
 end    
