0a92089e418d758c2d347fd6f089cb77dae6fb13
Hegnon <hegnondutra@gmail.com>
2023-05-25 06:56:24 -0300

    criando teste para bubble sort

---
+++ bublle_sort/spec/bublle_sort_spec.rb
@@ -0,0 +1,14 @@
+require 'bublle_sort'
+
+
+describe 'bublle_sort' do
+    it 'ordena corretamente o array' do
+    arr = [20,10,40,80,100,5,1]
+    sorted_arr = [1, 5, 10, 20, 40, 80, 100]
+
+    result = bubble_sort(arr)
+    expect(result).to eql(sorted_arr)
+    end      
+end
+
+    
