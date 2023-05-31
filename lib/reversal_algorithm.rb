f86a4035b0f4f2dd86e97c86c8793746cc1f300b
Hegnon <hegnondutra@gmail.com>
2023-05-31 06:17:05 -0300

    teste  Reversal Algorithm na gem concluido

--- lib/reversal_algorithm.rb
+++ lib/reversal_algorithm.rb
@@ -8,9 +8,11 @@ module ReversalAlgorithm
         temp = arr[i]
         arr[i] = arr[n-i-1]
         arr[n-i-1] = temp
-    end
+    end  
     return arr
+  end
 end  
+
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 n = 6
--- spec/reversal_algorithm_spec.rb
+++ spec/reversal_algorithm_spec.rb
@@ -2,14 +2,13 @@ require 'reversal_algorithm'
 
 RSpec.describe ReversalAlgorithm do
     include ReversalAlgorithm
-    
-    describe "reversal_algorithm" do        
+
+    describe 'reversal' do        
         let(:arr) { [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }
         let(:n) { 6 }
-        expected_output = [6, 5, 4, 3, 2, 1, 7, 8, 9, 10] 
-
+        
         it "Testando revelsal algorithm" do
-        expect(reversal(arr, n)).to eq([6, 5, 4, 3, 2, 1, 7, 8, 9, 10])
+        expect(reversal(arr, n)).to eql([6, 5, 4, 3, 2, 1, 7, 8, 9, 10])
         end        
     end
 end
