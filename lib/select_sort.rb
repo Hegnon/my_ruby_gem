7ebb5345c68bf357ce452aa08f3fbf068670d406
Hegnon <hegnondutra@gmail.com>
2023-06-01 19:32:04 -0300

    Adicionando select sort na gem

---
+++ lib/select_sort.rb
@@ -0,0 +1,30 @@
+require_relative "select_sort/version"
+
+module SelectSort
+  class Error < StandardError; end
+
+  def select (arr)
+    n = arr.length
+  
+    for i in 0..(n - 2)
+      min_index = i
+  
+      for j in (i + 1)..(n - 1)
+        if arr[j].nil? || (arr[min_index] && arr[j] < arr[min_index])
+          min_index = j
+        end
+      end
+  
+      if min_index != i
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+      end
+    end
+  
+    return arr
+  end
+  
+  # Exemplo de uso:
+  array = [64, 25, 12, 22, 11]
+  sorted_array = select (array)
+  puts sorted_array.inspect
+  
