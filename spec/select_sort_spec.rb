21f1b50d8482eb5c3020ae0bcc8c0efa25af64d9
Hegnon <hegnondutra@gmail.com>
2023-06-05 06:44:02 -0300

    teste para select sort concluido

--- spec/select_sort_spec.rb
+++ spec/select_sort_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe SelectSort do
     describe 'selection_sort' do      
       it 'ordena corretamente o array' do
         array = [64, 25, 12, 22, 11]
-        sorted_array = SelectSort.selection_sort(array)
+        sorted_array = selection_sort(array)
         expect(sorted_array).to eq([11, 12, 22, 25, 64]) 
       end
     end
