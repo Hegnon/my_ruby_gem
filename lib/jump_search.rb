22714d6ed49d24f028db81329a3dd696c1e7f2ac
Hegnon <hegnondutra@gmail.com>
2023-06-12 07:01:17 -0300

    teste para algoritimo jump search finalizado

--- lib/jump_search.rb
+++ lib/jump_search.rb
@@ -1,31 +1,31 @@
 require_relative "jump_search/version"
 
-module ReversalAlgorithm
+module JumpSearch
   class Error < StandardError; end
 
-  def jump(array, alvo)
+  def jump(array, target)
     n = array.length
     step = Math.sqrt(n).to_i
     prev = 0
 
-    while array[[step, n].min - 1] < alvo 
+    while array[[step, n].min - 1] < target 
         prev = step
         step += Math.sqrt(n).to_i
         return -1 if prev >= n
 
-        break if array[[step, n].min - 1] >= alvo
+        break if array[[step, n].min - 1] >= target
     end
 
-    while array[prev] < alvo
+    while array[prev] < target
         prev += 1
         return -1 if prev == [step, n].min    
     end
 
-    return prev if array[prev] == alvo
+    return prev if array[prev] == target
     return -1
 end
 
 array = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-alvo = 15
+target = 15
 
-result = jump(array, alvo)
+end
