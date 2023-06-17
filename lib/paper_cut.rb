897349a524a7d6aa89266f05e89c457f8a570b4c
Hegnon <hegnondutra@gmail.com>
2023-06-16 21:51:37 -0300

    build: structure paper cut

--- lib/paper_cut.rb
+++ lib/paper_cut.rb
@@ -1,28 +1,12 @@
-require_relative "select_sort/version"
+require_relative "paper_cut/version"
 
-module SelectSort
+module PaperCut
   class Error < StandardError; end
 
-  def selection_sort(arr)
-    n = arr.length
-  
-    for i in 0..(n - 2)
-      min_index = i
-  
-      for j in (i + 1)..(n - 1)
-        if arr[j].nil? || (arr[min_index] && arr[j] < arr[min_index])
-          min_index = j
-        end
-      end
-  
-      if min_index != i
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-      end
-    end
-  
-    return arr
+  def method_name
+    
   end
-end
   
-array = [64, 25, 12, 22, 11]
+
+  
 
